5c1eb08e29d4805573ddebedbafa1c333019d2ea
Mate Herber <mate.herber@outlook.com>
2022-08-30 11:03:44 +0200

    fix: use a proper derived state for the palette element

--- designkit/src/main/java/org/kredent/designkit/palette/Palette.kt
+++ designkit/src/main/java/org/kredent/designkit/palette/Palette.kt
@@ -14,6 +14,10 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
@@ -23,34 +27,39 @@ import org.kredent.designkit.theme.spacing
 
 @Composable
 fun Palette(modifier: Modifier = Modifier, onClick: (String, ULong, ULong) -> Unit) {
-    val items = listOf(
-        "primary" to (MaterialTheme.colorScheme.primary to MaterialTheme.colorScheme.onPrimary),
-        "onPrimary" to (MaterialTheme.colorScheme.onPrimary to MaterialTheme.colorScheme.primary),
-        "inversePrimary" to (MaterialTheme.colorScheme.inversePrimary to MaterialTheme.colorScheme.primary),
-        "primaryContainer" to (MaterialTheme.colorScheme.primaryContainer to MaterialTheme.colorScheme.onPrimaryContainer),
-        "onPrimaryContainer" to (MaterialTheme.colorScheme.onPrimaryContainer to MaterialTheme.colorScheme.primaryContainer),
-        "secondary" to (MaterialTheme.colorScheme.secondary to MaterialTheme.colorScheme.onSecondary),
-        "onSecondary" to (MaterialTheme.colorScheme.onSecondary to MaterialTheme.colorScheme.secondary),
-        "secondaryContainer" to (MaterialTheme.colorScheme.secondaryContainer to MaterialTheme.colorScheme.onSecondaryContainer),
-        "onSecondaryContainer" to (MaterialTheme.colorScheme.onSecondaryContainer to MaterialTheme.colorScheme.secondaryContainer),
-        "tertiary" to (MaterialTheme.colorScheme.tertiary to MaterialTheme.colorScheme.onTertiary),
-        "onTertiary" to (MaterialTheme.colorScheme.onTertiary to MaterialTheme.colorScheme.tertiary),
-        "tertiaryContainer" to (MaterialTheme.colorScheme.tertiaryContainer to MaterialTheme.colorScheme.onTertiaryContainer),
-        "onTertiaryContainer" to (MaterialTheme.colorScheme.onTertiaryContainer to MaterialTheme.colorScheme.tertiaryContainer),
-        "background" to (MaterialTheme.colorScheme.background to MaterialTheme.colorScheme.onBackground),
-        "onBackground" to (MaterialTheme.colorScheme.onBackground to MaterialTheme.colorScheme.background),
-        "error" to (MaterialTheme.colorScheme.error to MaterialTheme.colorScheme.onError),
-        "onError" to (MaterialTheme.colorScheme.onError to MaterialTheme.colorScheme.error),
-        "errorContainer" to (MaterialTheme.colorScheme.errorContainer to MaterialTheme.colorScheme.onErrorContainer),
-        "onErrorContainer" to (MaterialTheme.colorScheme.onErrorContainer to MaterialTheme.colorScheme.errorContainer),
-        "surface" to (MaterialTheme.colorScheme.surface to MaterialTheme.colorScheme.onSurface),
-        "onSurface" to (MaterialTheme.colorScheme.onSurface to MaterialTheme.colorScheme.surface),
-        "inverseSurface" to (MaterialTheme.colorScheme.inverseSurface to MaterialTheme.colorScheme.surface),
-        "surfaceVariant" to (MaterialTheme.colorScheme.surfaceVariant to MaterialTheme.colorScheme.onSurfaceVariant),
-        "onSurfaceVariant" to (MaterialTheme.colorScheme.onSurfaceVariant to MaterialTheme.colorScheme.surfaceVariant),
-        "surfaceTint" to (MaterialTheme.colorScheme.surfaceTint to MaterialTheme.colorScheme.surface),
-        "outline" to (MaterialTheme.colorScheme.outline to MaterialTheme.colorScheme.onPrimary),
-    )
+    val colorScheme = MaterialTheme.colorScheme
+    val items by remember(colorScheme) {
+        derivedStateOf {
+            listOf(
+                "primary" to (colorScheme.primary to colorScheme.onPrimary),
+                "onPrimary" to (colorScheme.onPrimary to colorScheme.primary),
+                "inversePrimary" to (colorScheme.inversePrimary to colorScheme.primary),
+                "primaryContainer" to (colorScheme.primaryContainer to colorScheme.onPrimaryContainer),
+                "onPrimaryContainer" to (colorScheme.onPrimaryContainer to colorScheme.primaryContainer),
+                "secondary" to (colorScheme.secondary to colorScheme.onSecondary),
+                "onSecondary" to (colorScheme.onSecondary to colorScheme.secondary),
+                "secondaryContainer" to (colorScheme.secondaryContainer to colorScheme.onSecondaryContainer),
+                "onSecondaryContainer" to (colorScheme.onSecondaryContainer to colorScheme.secondaryContainer),
+                "tertiary" to (colorScheme.tertiary to colorScheme.onTertiary),
+                "onTertiary" to (colorScheme.onTertiary to colorScheme.tertiary),
+                "tertiaryContainer" to (colorScheme.tertiaryContainer to colorScheme.onTertiaryContainer),
+                "onTertiaryContainer" to (colorScheme.onTertiaryContainer to colorScheme.tertiaryContainer),
+                "background" to (colorScheme.background to colorScheme.onBackground),
+                "onBackground" to (colorScheme.onBackground to colorScheme.background),
+                "error" to (colorScheme.error to colorScheme.onError),
+                "onError" to (colorScheme.onError to colorScheme.error),
+                "errorContainer" to (colorScheme.errorContainer to colorScheme.onErrorContainer),
+                "onErrorContainer" to (colorScheme.onErrorContainer to colorScheme.errorContainer),
+                "surface" to (colorScheme.surface to colorScheme.onSurface),
+                "onSurface" to (colorScheme.onSurface to colorScheme.surface),
+                "inverseSurface" to (colorScheme.inverseSurface to colorScheme.surface),
+                "surfaceVariant" to (colorScheme.surfaceVariant to colorScheme.onSurfaceVariant),
+                "onSurfaceVariant" to (colorScheme.onSurfaceVariant to colorScheme.surfaceVariant),
+                "surfaceTint" to (colorScheme.surfaceTint to colorScheme.surface),
+                "outline" to (colorScheme.outline to colorScheme.onPrimary),
+
+        )}
+    }
     LazyVerticalGrid(
         modifier = modifier,
         columns = GridCells.Fixed(2),
